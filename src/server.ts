c9a8745ecf8bd47360c1932c1aac1c839ee0e43f
fagneraureliano <fagner2aureliano@gmail.com>
2022-04-21 17:54:22 -0300

    Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import express, { response } from "express";
+import "reflect-metadata"
+import express from "express";
+
+import "./database"
 
 const app = express()
-
-app.get("/test", (req, res) => {
-  return  res.send("Olá NLW")
-})
-app.post("/test-post", (req,res)=>{
-  return res.send("Olá metodo post")
-} )
-
 
 app. listen(3000, () => console.log("Server is running..."))
